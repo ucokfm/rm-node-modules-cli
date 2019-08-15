ca0e950f6f5b79d2f06771341844e98995f879c8
Ucok Freddy <ucokfm@gmail.com>
2019-08-15 18:17:03 +0700

    feat: basic feature ask and remove

---
+++ index.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+const shell = require('shelljs');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const dirs = shell.find('.').filter(function(file) {
+  const count = file
+    .split('/')
+    .reduce((acc, cur) => cur === 'node_modules' ? acc + 1 : acc + 0, 0);
+  return file.match(/node_modules$/) && count === 1;
+});
+
+for (const dir of dirs) {
+  rl.question(`remove ${dir} (y/n) ? `, (answer) => {
+    if (answer === 'y') {
+      shell.rm('-rf', dir);
+    }
+    rl.close();
+  });
+}
